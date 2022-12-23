a8cafde88e19b204a8095b9571d84f2640ec3aae
Benjamin Kindle <benjaminkindle@yahoo.com>
2022-12-22 20:42:54 -0500

    fix: support knob controls and still auto refresh
    Fixes #10

--- examples/qwik-app/.storybook/main.ts
+++ examples/qwik-app/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/builder-vite';
 
 const config: StorybookConfig = {
-  addons: ['@storybook/addon-a11y'],
+  addons: ['@storybook/addon-essentials'],
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   framework: {
     name: 'storybook-framework-qwik',
