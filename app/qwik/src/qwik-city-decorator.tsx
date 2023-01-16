8c27c104b4167e777291dfe117a9cdaec5046845
Benjamin Kindle <benjaminkindle@yahoo.com>
2023-01-16 08:48:47 -0500

    refactor: use qwik jsx

--- app/qwik/src/qwik-city-decorator.tsx
+++ app/qwik/src/qwik-city-decorator.tsx
@@ -1,8 +1,7 @@
-import { JSXNode } from '@builder.io/qwik';
 import { QwikCityMockProvider } from '@builder.io/qwik-city';
 import { Decorator } from './types.js';
 
 /** Wraps story in QwikCityMockProvider */
-export const qwikCityDecorator: Decorator = (Story) =>
-  // Something is out of sync with the JSXNode generated here and the type expected by Decorator
-  (<QwikCityMockProvider>{Story()}</QwikCityMockProvider>) as JSXNode;
+export const qwikCityDecorator: Decorator = (Story) => (
+  <QwikCityMockProvider>{Story()}</QwikCityMockProvider>
+);
