0150c3659516f1b785a4cecb9581afd9acfefb0f
Benjamin Kindle <benjaminkindle@yahoo.com>
2023-09-02 11:44:57 -0400

    fix: compatibility with qwik 1.2 (#64)
    server error: Unexpected `FunctionDeclaration` can happen
    because we were removing mdx transforms only when the plugin is in the array.
    Starting in qwik 1.2, the plugin may be in an array inside another array.
    
    Fixes #63

--- packages/storybook-framework-qwik/src/preset.ts
+++ packages/storybook-framework-qwik/src/preset.ts
@@ -33,11 +33,19 @@ export const viteFinal: StorybookConfig["viteFinal"] = async (
 
   // Qwik-city plugin may be used in apps, but it has mdx stuff that conflicts with Storybook mdx
   // we'll try to only remove the transform code (where the mdx stuff is), and keep everything else.
-  config.plugins = config.plugins.map((plugin: Plugin) =>
-    plugin.name === "vite-plugin-qwik-city"
+  config.plugins = config.plugins.map((plugin: Plugin) => {
+    // the plugin may be standalone (Qwik <1.2), or in an array (Qwik >=1.2)
+    if (Array.isArray(plugin)) {
+      return plugin.map((subPlugin: Plugin) => {
+        return subPlugin.name === "vite-plugin-qwik-city"
+          ? { ...subPlugin, transform: () => null as any }
+          : subPlugin;
+      });
+    }
+    return plugin.name === "vite-plugin-qwik-city"
       ? { ...plugin, transform: () => null as any }
-      : plugin
-  );
+      : plugin;
+  });
 
   return config;
 };
