2670d63952d24c449b3617fef4e816893a645803
Benjamin Kindle <benjaminkindle@yahoo.com>
2023-12-09 10:10:09 -0500

    add tests for useVisibleTask
    also improves existing tests and update to newer version of qwik

---
+++ packages/qwik-app/src/components/use-visible-task-component/use-visible-task-component.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from "../../../../storybook-framework-qwik/dist";
+import { within } from "@storybook/testing-library";
+import { UseVisibleTaskComponent } from "./use-visible-task-component";
+
+
+export default {
+  title: "Visible Task Component",
+  component: UseVisibleTaskComponent,
+} as Meta;
+
+export const Default: StoryObj = {
+  play: async ({canvasElement})=> {
+    const canvas = within(canvasElement);
+    
+    await canvas.findByText('visible task ran? Yes')
+  }
+};
