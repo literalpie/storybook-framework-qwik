5074d2cda98b0001fa5c4b7e4ebf0656686a3754
Benjamin Kindle <benjaminkindle@yahoo.com>
2024-02-17 12:13:09 -0500

    chore: update dependencies
    Storybook to 7.6.x and most other dependencies.
    Not updating qwik or storybook test-runner to latest because of different issues

--- packages/qwik-app/.storybook/main.ts
+++ packages/qwik-app/.storybook/main.ts
@@ -1,7 +1,7 @@
 import { StorybookConfig } from "storybook-framework-qwik";
 
 const config: StorybookConfig = {
-  addons: ["@storybook/addon-essentials"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
   stories: ["../src/**/*.docs.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   framework: {
     name: "storybook-framework-qwik",
