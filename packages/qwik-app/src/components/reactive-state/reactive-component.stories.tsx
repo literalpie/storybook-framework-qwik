ba997febec1f91366bcfb7d3ba56550e5522b6d7
Benjamin Kindle <benjaminkindle@yahoo.com>
2025-09-27 11:11:18 -0400

    remove yarn stuff

--- packages/qwik-app/src/components/reactive-state/reactive-component.stories.tsx
+++ packages/qwik-app/src/components/reactive-state/reactive-component.stories.tsx
@@ -19,6 +19,9 @@ export default {
   args: { state: { count: 1 } },
 } as Meta<ReactiveComponentProps>;
 
+// Note: it seems like this fails when testing locally (props are immutable), but passes in CI. 
+// Not sure which one is correct. 
+// The goal is for this component to only mutate a store, which is shown in Qwik docs
 export const Default: StoryObj<ReactiveComponentProps> = {};
 export const Args: StoryObj<ReactiveComponentProps> = {
   args: { state: { count: 7 } },
