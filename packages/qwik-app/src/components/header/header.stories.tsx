ac6c3091e45ec1efc79f242232bf2f9273275809
Jake Holland <contact@hollandjake.com>
2026-05-09 11:28:56 +0100

    feat: add parameters to configure qwik router mock

--- packages/qwik-app/src/components/header/header.stories.tsx
+++ packages/qwik-app/src/components/header/header.stories.tsx
@@ -6,9 +6,19 @@ export default {
   component: Header,
   args: {},
   tags: ["docsPage"],
+  parameters: {
+    qwikCity: {
+      url: "http://example.com"
+    }
+  },
 } as Meta<HeaderProps>;
 
 export const Default: StoryObj<HeaderProps> = {};
 export const WithTitle: StoryObj<HeaderProps> = {
   args: { title: "Test Title" },
+  parameters: {
+    qwikCity: {
+      url: "http://qwik.dev"
+    }
+  }
 };
--- packages/storybook-framework-qwik/src/qwik-city-decorator.tsx
+++ packages/storybook-framework-qwik/src/qwik-city-decorator.tsx
@@ -2,6 +2,6 @@ import { QwikRouterMockProvider} from "@qwik.dev/router";
 import { Decorator } from "./types.js";
 
 /** Wraps story in QwikRouterMockProvider */
-export const qwikCityDecorator: Decorator = (Story) => (
-  <QwikRouterMockProvider>{Story()}</QwikRouterMockProvider>
+export const qwikCityDecorator: Decorator = (Story, {parameters}) => (
+  <QwikRouterMockProvider {...parameters.qwikCity}>{Story()}</QwikRouterMockProvider>
 );
