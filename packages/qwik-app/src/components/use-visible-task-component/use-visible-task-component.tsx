0d535f0d6fbfcb19213b3d78c62d1f4f9f09f9b4
Benjamin Kindle <20398475+literalpie@users.noreply.github.com>
2026-05-09 08:12:25 -0400

    fmt

--- packages/qwik-app/src/components/use-visible-task-component/use-visible-task-component.tsx
+++ packages/qwik-app/src/components/use-visible-task-component/use-visible-task-component.tsx
@@ -3,9 +3,12 @@ import { component$, useSignal, useVisibleTask$ } from "@qwik.dev/core";
 export const UseVisibleTaskComponent = component$(() => {
   const visibleTaskRan = useSignal(false);
   // eslint-disable-next-line qwik/no-use-visible-task
-  useVisibleTask$(() => {
-    // eslint-disable-next-line qwik/valid-lexical-scope
-    visibleTaskRan.value = true;
-  }, {strategy: 'document-ready'});
+  useVisibleTask$(
+    () => {
+      // eslint-disable-next-line qwik/valid-lexical-scope
+      visibleTaskRan.value = true;
+    },
+    { strategy: "document-ready" },
+  );
   return <>visible task ran? {visibleTaskRan.value ? "Yes" : "No"}</>;
 });
